0550469249db4a6d4d1d0d418a6c32dd94063291
M A Kalam Sharif <makalamnirjon@gmail.com>
2026-08-08 11:30:00 +0600

    Trim CLIENT_URL env var to prevent invalid CORS header errors

--- server/src/app.js
+++ server/src/app.js
@@ -11,8 +11,10 @@ const { notFoundHandler, errorHandler } = require('./middleware/error.middleware
 
 const app = express();
 
+const clientUrl = process.env.CLIENT_URL?.trim().replace(/\/$/, '');
+
 app.use(helmet());
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cors({ origin: clientUrl || true, credentials: true }));
 app.use(morgan('dev'));
 app.use(express.json());
 
